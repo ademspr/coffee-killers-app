e0db6f4b63a7bb9df1067ddac77833ded83327d6
Ademir Pereira <ademirspr@live.com>
2019-02-28 22:38:30 -0300

    intusiasmo

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import {Hello} from './src/components/Hello';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Hello name="Guizera"  enthusiasmLevel={20}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
